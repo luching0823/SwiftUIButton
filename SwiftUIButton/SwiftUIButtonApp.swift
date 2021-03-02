216f0978b44231429167b89bbdff01e21681a361
廖昱晴 <pulin.liao@ctbcsec.com>
2021-03-02 16:39:21 +0800

    Initial Commit

---
+++ SwiftUIButton/SwiftUIButtonApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIButtonApp.swift
+//  SwiftUIButton
+//
+//  Created by 廖昱晴 on 2021/3/2.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIButtonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
