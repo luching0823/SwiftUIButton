efcc8a926f504caf05cb91523678cfa71955fc57
廖昱晴 <pulin.liao@ctbcsec.com>
2021-03-02 17:25:49 +0800

    init SwiftUI Button

--- SwiftUIButton/ContentView.swift
+++ SwiftUIButton/ContentView.swift
@@ -9,8 +9,93 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
+//        Button(action: {
+//            print("Hello World Tapped!")
+//        }) {
+//            Text("Hello World")
+//                .foregroundColor(.white)
+//                .font(.title)
+//                .fontWeight(.bold)
+//                .padding()
+//                .background(Color.purple)
+//                .cornerRadius(50)
+//                .padding()
+//                .overlay(
+//                RoundedRectangle(cornerRadius: 50)
+//                    .stroke(Color.purple, lineWidth: 5)
+//                )
+//        }
+        VStack {
+//            Button(action: {
+//                print("Share tapped!")
+//            }) {
+//                Label(
+//                    title: { Text("Share")
+//                        .fontWeight(.semibold)
+//                        .font(.title)
+//                    },
+//                    icon: { Image(systemName: "square.and.arrow.up")
+//                        .font(.title)
+//                    }
+//                )
+//            }.buttonStyle(GraientBackgroundStyle())
+//
+//            Button(action: {
+//                print("Edit tapped!")
+//            }) {
+//                Label(
+//                    title: { Text("Edit")
+//                        .fontWeight(.semibold)
+//                        .font(.title)
+//                    },
+//                    icon: { Image(systemName: "square.and.pencil")
+//                        .font(.title)
+//                    }
+//                )
+//            }.buttonStyle(GraientBackgroundStyle())
+//
+//            Button(action: {
+//                print("Delete button tapped!")
+//            }) {
+//                Label(
+//                    title: { Text("Delete")
+//                        .fontWeight(.semibold)
+//                        .font(.title)
+//                    },
+//                    icon: { Image(systemName: "trash")
+//                        .font(.title)
+//                    }
+//                )
+//            }.buttonStyle(GraientBackgroundStyle())
+            
+            Button(action: {
+                print("press")
+            }) {
+                Text("+")
+                    .foregroundColor(.white)
+                    .font(.title)
+                    .fontWeight(.bold)
+            }.buttonStyle(GraientBackgroundStyle())
+            
+        }
+        
+    }
+}
+
+struct GraientBackgroundStyle: ButtonStyle {
+    func makeBody(configuration: Configuration) -> some View {
+        configuration.label
+//            .frame(minWidth: 0, maxWidth: .infinity)
+//            .padding()
+//            .foregroundColor(.white)
+//            .background(LinearGradient(gradient: Gradient(colors: [Color("DarkGreen"), Color("LightGreen")]), startPoint: .leading, endPoint: .trailing))
+//            .cornerRadius(40)
+//            .padding(.horizontal, 20)
+//            .scaleEffect(configuration.isPressed ? 0.9: 1.0)
             .padding()
+            .background(Color.purple)
+            .clipShape(Circle())
+            .rotationEffect(configuration.isPressed ? .degrees(45) : .degrees(0))
     }
 }
 
